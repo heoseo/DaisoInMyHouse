a60dbf9eed1f27db374087edf3c578cd31ee9622
heoseo <hsyoon1799@naver.com>
2020-09-09 02:07:02 +0900

    화면 띄우기 일부 구현

--- app/src/main/java/com/example/daisoinmyhouse/MyPageFragment.java
+++ app/src/main/java/com/example/daisoinmyhouse/MyPageFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,12 +19,16 @@ import androidx.fragment.app.Fragment;
 public class MyPageFragment extends Fragment {
 
     ImageButton btnSetting;
+    LinearLayout llSettingMyArea;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_mypage, container, false);
+
+
+        // 설정 버튼 -> SettingActivity 띄우기
         btnSetting = rootView.findViewById(R.id.btn_setting);
         btnSetting.setOnClickListener( new View.OnClickListener(){
             @Override
@@ -32,7 +37,24 @@ public class MyPageFragment extends Fragment {
                 getContext().startActivity(intent);
             }
         });
-        return rootView;
 
+
+        // 내 동네 설정 버튼 -> SettingMyAreaActivity 띄우기
+        llSettingMyArea = rootView.findViewById(R.id.ll_setting_my_area);
+        llSettingMyArea.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), SettingMyAreaActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+        return rootView;
     }
 }
--- app/src/main/java/com/example/daisoinmyhouse/WriteNewItemFragment.java
+++ app/src/main/java/com/example/daisoinmyhouse/WriteNewItemFragment.java
@@ -1,9 +1,12 @@
 package com.example.daisoinmyhouse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +14,23 @@ import androidx.fragment.app.Fragment;
 
 public class WriteNewItemFragment extends Fragment {
 
+    TextView btnSelectCategory;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_write_new_item, container, false);
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_write_new_item, container, false);
+        btnSelectCategory = rootView.findViewById(R.id.tv_category);
+        btnSelectCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CategoryInWriteActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
+
+
+        return rootView;
     }
 }
