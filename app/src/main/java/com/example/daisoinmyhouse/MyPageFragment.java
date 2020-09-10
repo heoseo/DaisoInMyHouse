d1279df5209f96ca7dcd8215a1115ceb9b8b219b
labasimys <hilight0224@naver.com>
2020-09-10 15:55:44 +0900

    카톡 공유 세팅

--- app/src/main/java/com/example/daisoinmyhouse/MyPageFragment.java
+++ app/src/main/java/com/example/daisoinmyhouse/MyPageFragment.java
@@ -1,19 +1,28 @@
 package com.example.daisoinmyhouse;
 
-import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.kakao.kakaolink.v2.KakaoLinkResponse;
+import com.kakao.kakaolink.v2.KakaoLinkService;
+import com.kakao.message.template.ButtonObject;
+import com.kakao.message.template.ContentObject;
+import com.kakao.message.template.FeedTemplate;
+import com.kakao.message.template.LinkObject;
+import com.kakao.network.ErrorResult;
+import com.kakao.network.callback.ResponseCallback;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyPageFragment extends Fragment {
 
@@ -32,10 +41,6 @@ public class MyPageFragment extends Fragment {
                 getContext().startActivity(intent);
             }
         });
-
-
-
         return rootView;
-
     }
 }
