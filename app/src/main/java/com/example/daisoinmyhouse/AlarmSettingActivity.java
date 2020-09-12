20cf1f70d14562a1f74b6c31e24c51bfc211078b
heoseo <hsyoon1799@naver.com>
2020-09-12 22:33:19 +0900

    키워드 알림 일부 구현

--- app/src/main/java/com/example/daisoinmyhouse/AlarmSettingActivity.java
+++ app/src/main/java/com/example/daisoinmyhouse/AlarmSettingActivity.java
@@ -1,7 +1,10 @@
 package com.example.daisoinmyhouse;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AlarmSettingActivity extends AppCompatActivity {
 
     Switch sbAlarm, sbChatAlarm, sbReplyitem, sbWish, sbReplyStore, sbDelivery, sbDeliveryComplete;
+    LinearLayout btnKeywordAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +31,30 @@ public class AlarmSettingActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b){
-                    sbChatAlarm.setEnabled(true);
-                    sbReplyitem.setEnabled(true);
-                    sbWish.setEnabled(true);
-                    sbReplyStore.setEnabled(true);
-                    sbDelivery.setEnabled(true);
-                    sbDeliveryComplete.setEnabled(true);
+                    sbChatAlarm.setChecked(true);
+                    sbReplyitem.setChecked(true);
+                    sbWish.setChecked(true);
+                    sbReplyStore.setChecked(true);
+                    sbDelivery.setChecked(true);
+                    sbDeliveryComplete.setChecked(true);
                 }
                 else{
-                    sbChatAlarm.setEnabled(false);
-                    sbReplyitem.setEnabled(false);
-                    sbWish.setEnabled(false);
-                    sbReplyStore.setEnabled(false);
-                    sbDelivery.setEnabled(false);
-                    sbDeliveryComplete.setEnabled(false);
+                    sbChatAlarm.setChecked(false);
+                    sbReplyitem.setChecked(false);
+                    sbWish.setChecked(false);
+                    sbReplyStore.setChecked(false);
+                    sbDelivery.setChecked(false);
+                    sbDeliveryComplete.setChecked(false);
                 }
+            }
+        });
+
+        btnKeywordAlarm = (LinearLayout)findViewById(R.id.btn_keyword);
+        btnKeywordAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), KeywordAlarmActivity.class);
+                startActivity(intent);
             }
         });
     }
