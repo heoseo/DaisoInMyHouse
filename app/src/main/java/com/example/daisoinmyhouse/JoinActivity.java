86ff5588dc6fb7d38844359fa16f162d5ad2c735
heoseo <hsyoon1799@naver.com>
2020-10-07 00:19:33 +0900

    DB 연동 완료

--- app/src/main/java/com/example/daisoinmyhouse/JoinActivity.java
+++ app/src/main/java/com/example/daisoinmyhouse/JoinActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,6 +34,7 @@ public class JoinActivity extends AppCompatActivity {
 
                     RegisterActivity task = new RegisterActivity();
                     result = task.execute(id, pw).get();
+                    Toast.makeText(getApplicationContext(),"회원가입이 완료되었습니다!", Toast.LENGTH_LONG);
                 } catch (Exception e) {
                     Log.i("DBtest", ".....ERROR.....!");
                 }
