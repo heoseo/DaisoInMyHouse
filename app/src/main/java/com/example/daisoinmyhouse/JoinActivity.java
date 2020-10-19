933a6781870c05ab70b3f29bed007bb56214113c
heoseo <hsyoon1799@naver.com>
2020-10-20 01:24:26 +0900

    20201019 주소검색

--- app/src/main/java/com/example/daisoinmyhouse/JoinActivity.java
+++ app/src/main/java/com/example/daisoinmyhouse/JoinActivity.java
@@ -1,17 +1,19 @@
 package com.example.daisoinmyhouse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JoinActivity extends AppCompatActivity {
 
-    Button joinBtn;
+    private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
+
+    Button joinBtn, zipCodeBtn;
     EditText nameet, idet, pwet, pwet_confirm, emailet, phoneet, addresset;
 
     @Override
@@ -20,6 +22,8 @@ public class JoinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_join);
 
         joinBtn = (Button)findViewById(R.id.activity_join_join_btn);
+        zipCodeBtn = (Button)findViewById(R.id.btn_zip_code);
+
         nameet = (EditText)findViewById(R.id.et_name);
         idet = (EditText)findViewById(R.id.et_id);
         pwet = (EditText)findViewById(R.id.et_pw);
@@ -27,6 +31,14 @@ public class JoinActivity extends AppCompatActivity {
         emailet = (EditText)findViewById(R.id.et_email);
         phoneet = (EditText)findViewById(R.id.et_phone);
         addresset = (EditText)findViewById(R.id.et_address);
+
+        zipCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FindAddressActivity.class);
+                startActivityForResult(intent, SEARCH_ADDRESS_ACTIVITY);
+            }
+        });
 
         joinBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -38,15 +50,12 @@ public class JoinActivity extends AppCompatActivity {
                     String email = emailet.getText().toString();
                     String phone = phoneet.getText().toString();
                     String address = addresset.getText().toString();
-
                     RegisterActivity task = new RegisterActivity();
                     result = task.execute(name, id, pw, email, phone, address).get();
-
                     // 빈칸이 있는지 검사사
-                   if(name.getBytes().length <=0 && id.getBytes().length <=0 && pw.getBytes().length <=0 && email.getBytes().length <=0 && phone.getBytes().length <=0 && address.getBytes().length <=0){
+                    if(name.getBytes().length <=0 && id.getBytes().length <=0 && pw.getBytes().length <=0 && email.getBytes().length <=0 && phone.getBytes().length <=0 && address.getBytes().length <=0){
                         Toast.makeText(getApplicationContext(), "모든 입력창을 입력해주세요!", Toast.LENGTH_LONG).show();
                     }else{
-
                         Toast.makeText(getApplicationContext(),"회원가입이 완료되었습니다!", Toast.LENGTH_LONG).show();
                         finish();
                     }
@@ -55,6 +64,20 @@ public class JoinActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        super.onActivityResult(requestCode, resultCode, intent);
+        switch (requestCode) {
+            case SEARCH_ADDRESS_ACTIVITY:
+                if (resultCode == RESULT_OK) {
+                    String data = intent.getExtras().getString("data");
+                    if (data != null) {
+                        addresset.setText(data);
+                    }
+                }
+                break;
+        }
     }
 }
