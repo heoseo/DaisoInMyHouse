3d9cf894eebd549126841f113b3493758a6bf35b
heoseo <hsyoon1799@naver.com>
2020-10-27 01:59:07 +0900

    .

---
+++ app/src/main/java/com/example/daisoinmyhouse/SaveAreaInDBActivity.java
@@ -0,0 +1,55 @@
+package com.example.daisoinmyhouse;
+
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+public class RegisterActivity extends AsyncTask<String, Void, String> {
+    String sendMsg, receiveMsg;
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            String str;
+            // 접속할 서버 주소 (이클립스에서 android.jsp 실행시 웹브라우저 주소)
+            URL url = new URL("http://daisoinmyhouse.cafe24.com/joinAction.jsp");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");
+            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+            // 전송할 데이터. GET 방식으로 작성
+            sendMsg = "name=" + strings[0] +"&id=" + strings[1] + "&pw=" + strings[2]
+                    + "&email=" + strings[3] + "&phone=" + strings[4] + "&address=" + strings[5];
+            osw.write(sendMsg);
+            osw.flush();
+            //jsp와 통신 성공 시 수행
+            if (conn.getResponseCode() == conn.HTTP_OK) {
+                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                BufferedReader reader = new BufferedReader(tmp);
+                StringBuffer buffer = new StringBuffer();
+                // jsp에서 보낸 값을 받는 부분
+                while ((str = reader.readLine()) != null) {
+                    buffer.append(str);
+                }
+                receiveMsg = buffer.toString();
+                Log.i("테스트", receiveMsg);
+            } else {
+                // 통신 실패
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //jsp로부터 받은 리턴 값
+        return receiveMsg;
+    }
+
+} 
