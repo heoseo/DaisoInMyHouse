170ac1e40584cd2996fef47769436f496d5426c7
heoseo <hsyoon1799@naver.com>
2020-11-08 20:30:25 +0900

    want글쓰기 수정

--- app/src/main/java/com/example/daisoinmyhouse/WriteWantItemFragment.java
+++ app/src/main/java/com/example/daisoinmyhouse/WriteWantItemFragment.java
@@ -1,22 +1,14 @@
 package com.example.daisoinmyhouse;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -34,12 +26,11 @@ public class WriteWantItemFragment extends Fragment {
 
     private static final int SEARCH_LOCATION_ACTIVITY = 1000;
     private final int REQ_CODE_LOCAION = 50;
-    EditText product_name,conttent,taag;
-    TextView cattegory;
+    EditText et_want_name, et_want_content;
     Button writeBtn;
     TextView btnSetLocation;
     MainActivity activity;
-    Spinner spinner;
+    Spinner want_spinner;
     LinearLayout btn_otherwrite;
 
     public static WriteWantItemFragment newInstance(){
@@ -52,22 +43,12 @@ public class WriteWantItemFragment extends Fragment {
 
         activity = (MainActivity) getActivity();
 
-
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_write_want_item, container, false);
 
-        product_name =rootView.findViewById(R.id.et_product_name);
-        conttent = rootView.findViewById(R.id.et_product_content);
+        et_want_name =rootView.findViewById(R.id.et_want_name);
+        et_want_content = rootView.findViewById(R.id.et_want_content);
 
-        spinner = rootView.findViewById(R.id.spinner_product_cate);
-/*        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });*/
+        want_spinner = rootView.findViewById(R.id.spinner_want_cate);
 
 
 
@@ -108,29 +89,25 @@ public class WriteWantItemFragment extends Fragment {
 
                     String user_id = StaticUserInformation.userID;
 
-                    String want_name = product_name.getText().toString();
-                    String want_cate = cattegory.getText().toString();
+                    String want_name = et_want_name.getText().toString();
+                    String want_cate = want_spinner.getSelectedItem().toString();
                     String location = btnSetLocation.getText().toString();
-                    String want_content = conttent.getText().toString();
-                    String time = conttent.getText().toString();
-                    wantWriteActivity task =new wantWriteActivity();
+                    String want_content = et_want_content.getText().toString();
 
-                    String result = task.execute(user_id,want_cate,want_name, want_content,location,time).get();
+                    WantWriteAction task = new WantWriteAction();
+
                     // 빈칸이 있는지 검사사
                     if(want_name.getBytes().length <=0 || want_cate.getBytes().length <= 0 || want_content.getBytes().length <= 0 ){
                         Toast.makeText(activity.getApplicationContext(), "모든 입력창을 입력해주세요!", Toast.LENGTH_LONG).show();
                     }else{
+                        String result = task.execute(user_id, want_cate, want_name, want_content, location).get();
                         Toast.makeText(activity.getApplicationContext(), result, Toast.LENGTH_LONG).show();
-                        activity.finish();
                     }
                 } catch (Exception e) {
                     Log.i("DBtest", ".....ERROR.....!");
                 }
             }
         });
-
-
-
 
 
         return rootView;
@@ -146,14 +123,14 @@ public class WriteWantItemFragment extends Fragment {
         }
 
 
-        if (requestCode == REQ_CODE_LOCAION) {
-            String resultMsg = intent.getStringExtra("result_msg");
-            btnSetLocation.setText(resultMsg);
-
-//            Toast.makeText(MainActivity.this, "결과 : " + resultMsg, Toast.LENGTH_SHORT).show();
-        } else {
-//            Toast.makeText(MainActivity.this, "REQUEST_ACT가 아님", Toast.LENGTH_SHORT).show();
-        }
+//        if (requestCode == REQ_CODE_LOCAION) {
+//            String resultMsg = intent.getStringExtra("result_msg");
+//            btnSetLocation.setText(resultMsg);
+//
+////            Toast.makeText(MainActivity.this, "결과 : " + resultMsg, Toast.LENGTH_SHORT).show();
+//        } else {
+////            Toast.makeText(MainActivity.this, "REQUEST_ACT가 아님", Toast.LENGTH_SHORT).show();
+//        }
 
         if(requestCode == SEARCH_LOCATION_ACTIVITY) {
             if (resultCode == RESULT_OK){
